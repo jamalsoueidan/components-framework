fe581045a5640c46ccf33174d747765171537532
Jamal Soueidan <jse@kamstrup.dk>
2016-10-21 10:08:26 +0200

    Get Store comment

--- src/build.js
+++ src/build.js
@@ -2,6 +2,9 @@ import configureRoutes from './config/routes';
 import configureStore from './config/store'
 import start from './config/start'
 
+/*
+  var store = KAF.getStore();
+ */
 const getStore = () => {
   return configureStore();
 }
